6c8725b096c6d73ec0e82c92ced463566bfd7e19
bibekjoshi01 <bibekjoshi34@gmail.com>
2024-12-23 18:14:46 +0545

    user jazzmin

--- src/user/admin.py
+++ src/user/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from .models import User
+from django.urls import reverse
+from django.utils.html import format_html
 
 from rest_framework_simplejwt.token_blacklist.models import (
     BlacklistedToken,
@@ -12,67 +14,54 @@ from rest_framework_simplejwt.token_blacklist.models import (
 admin.site.unregister(BlacklistedToken)
 admin.site.unregister(OutstandingToken)
 
-class UserAdmin(BaseUserAdmin):
+
+class UserAdmin(admin.ModelAdmin):
     model = User
     list_display = (
-        "email",
-        "username",
+        "photo",
         "full_name",
-        "is_active",
-        "is_staff",
-        "is_superuser",
+        "email",
+        "phone_no",
+        "username",
+        "last_login",
         "date_joined",
-        "auth_provider",
-    )
-    list_filter = (
         "is_active",
-        "is_staff",
-        "is_superuser",
-        "auth_provider",
-        "is_email_verified",
-        "is_phone_verified",
+        "action_buttons",
     )
-    search_fields = ("email", "username", "first_name", "last_name", "phone_no")
-    ordering = ("-date_joined",)
-
-    fieldsets = (
-        (
-            _("Personal Info"),
-            {"fields": ("email", "username", "full_name", "phone_no")},
-        ),
-        (_("Profile"), {"fields": ("photo", "bio", "auth_provider")}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                )
-            },
-        ),
-        (_("Important dates"), {"fields": ("date_joined", "updated_at")}),
-    )
-
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "username", "password1", "password2"),
-            },
-        ),
-    )
-
+    exclude = [
+        "uuid",
+        "created_at",
+        "updated_at",
+        "created_by",
+        "is_archived",
+    ]
+    list_filter = []
     readonly_fields = ("date_joined", "updated_at")
-    filter_horizontal = ("groups", "user_permissions")
 
     def full_name(self, obj):
         return obj.full_name or "N/A"
 
     full_name.short_description = "Full Name"
+
+    def action_buttons(self, obj):
+        edit_url = reverse("admin:user_user_change", args=[obj.id])
+        return format_html(
+            '<a class="button" href="{}">'
+            '<i class="fas fa-edit"></i> Edit</a>',
+            edit_url,
+        )
+
+    action_buttons.short_description = "Actions"
+    action_buttons.allow_tags = True
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 # Register the customized UserAdmin
