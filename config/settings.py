9c800612f68b15fe5f1aefde317d9ae6e7346fa6
bibekjoshi01 <bibekjoshi34@gmail.com>
2025-01-11 09:46:43 +0545

    fix: postgres sql configured

--- config/settings.py
+++ config/settings.py
@@ -8,10 +8,10 @@ env = environ.Env()
 environ.Env.read_env(str(BASE_DIR / ".env"))
 
 
-# CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS")
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORGINS = env.list("CORS_ALLOWED_ORIGINS")
-# CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://another-origin.com"]
+CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS")
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORGINS = env.list("CORS_ALLOWED_ORIGINS")
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://another-origin.com"]
 
 
 # GENERAL
@@ -98,8 +98,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # ------------------------------------------------------------------------------
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     },
 }
 
